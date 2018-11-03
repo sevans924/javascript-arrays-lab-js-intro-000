e2c1766f41e1cb6820154ceef27fd96e2bc87314
Sarah Elzinga <sevans924@gmail.com>
2018-11-03 13:51:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }// Add your functions and code here
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
